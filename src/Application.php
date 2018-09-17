759c0dc82ba15701b847c96513aa6bdec337db00
Andre Pukk <andrep615@hotmail.com>
2018-09-17 12:30:29 +0300

    Separate Application.php logic into more different methods

--- src/Application.php
+++ src/Application.php
@@ -15,6 +15,16 @@ class Application extends SilexApplication
     {
         parent::__construct($values);
 
+        $this->configureServices();
+        $this->createDBTables();
+        $this->configureControllers();
+    }
+
+    /**
+     * Config app options and register services.
+     */
+    private function configureServices()
+    {
         $this['debug'] = true;
 
         $this->register(new TwigServiceProvider(), [
@@ -28,8 +38,13 @@ class Application extends SilexApplication
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
+    }
 
-        // Creating a table if it doesn't exist yet
+    /**
+     * Creates all needed tables to database if they don't exist.
+     */
+    private function createDBTables()
+    {
         if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -46,7 +61,13 @@ class Application extends SilexApplication
                 payingMethod VARCHAR(10) NOT NULL
             );");
         }
+    }
 
+    /**
+     * Define all used routes and connect a route to its controller.
+     */
+    private function configureControllers()
+    {
         $this->get('/bookings/create', function () {
             return $this['twig']->render('base.html.twig');
         });
