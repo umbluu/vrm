8f9d5cdd373a47797b64c3dd88671edbb9f94605
Andre Pukk <andrep615@hotmail.com>
2018-09-17 15:16:06 +0300

    Save form data to the database if form is submitted

--- src/Application.php
+++ src/Application.php
@@ -81,8 +81,10 @@ class Application extends SilexApplication
         $this
             ->match('/bookings/create', new CreateBookingController(
                 $this['form.factory'],
-                $this['twig']
+                $this['twig'],
+                $this['db']
             ))
+            ->method('GET|POST')
         ;
     }
 }
--- src/Controllers/CreateBookingController.php
+++ src/Controllers/CreateBookingController.php
@@ -2,6 +2,7 @@
 
 namespace BookingApp\Controllers;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
@@ -11,16 +12,22 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class CreateBookingController
 {
-    public function __construct(FormFactory $formFactory, \Twig_Environment $twigEnv)
-    {
+    public function __construct(
+        FormFactory $formFactory,
+        \Twig_Environment $twigEnv,
+        Connection $dbConnection
+    ) {
         $this->formFactory = $formFactory;
         $this->twigEnv = $twigEnv;
+        $this->dbConnection = $dbConnection;
     }
 
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $form = $this->formFactory->createBuilder(FormType::class)
             ->add('firstName', TextType::class, ['required' => true])
@@ -57,6 +64,29 @@ class CreateBookingController
             ->add('submit', SubmitType::class, ['label' => 'Send booking'])
             ->getForm()
         ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $st = $this->dbConnection->executeQuery("INSERT INTO bookings (firstName, lastName, phone, email, birthday, startDate, endDate, arrivalTime, nrOfPeople, payingMethod, additionalInformation)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+                $data['firstName'],
+                $data['lastName'],
+                $data['phone'],
+                $data['email'],
+                $data['birthday']->format('Y-m-d H:i:s'),
+                $data['startDate']->format('Y-m-d H:i:s'),
+                $data['endDate']->format('Y-m-d H:i:s'),
+                $data['arrivalTime']->format('H:i:s'),
+                $data['nrOfPeople'],
+                $data['payingMethod'],
+                $data['additionalInformation']
+            ]);
+
+            return new RedirectResponse($request->getUri());
+        }
 
         return $this->twigEnv->render('form.html.twig', ['form' => $form->createView()]);
     }
